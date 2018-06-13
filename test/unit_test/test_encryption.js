86139f073322f09714e82b5814efa5c52876d6e0
juwitaw <juwita.winadwiastuti@dattabot.io>
2018-06-13 10:37:39 +0700

    done shamir

--- test/unit_test/test_encryption.js
+++ test/unit_test/test_encryption.js
@@ -2,15 +2,20 @@ var assert = require('assert');
 const Encryption = require('../../lib/encryption.js');
 
 describe('Encryption', function() {
+  let shares;
+  let encrypt;
+  beforeEach(function(){
+    encrypt = new Encryption()
+  });
   describe('Shamir Secret Sharing Schema', function() {
     it('should share shamir keys', function() {
       var words = "this is test";
-      var shares = secrets.share(key, 10, 5);
-      assert.equal(shares.lengts, 10);
+      shares = encrypt.generate_shamirsss(words, 5, 10);
+      assert.equal(shares.lengths, 10);
     });
     it('combine keys to produce "this is test"', function() {
-      var words = secrets.combine(shares.slice(1,5));
-      assert.equal(keys[0], "testing");
+      var words = encrypt.combine_shamirsss(shares.slice(1,6));
+      assert.equal(words, "this is test");
     });
   });
 });
